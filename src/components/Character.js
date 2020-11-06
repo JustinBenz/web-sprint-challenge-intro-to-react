89d0c7e7d36447c7f98f37c3578ff786e76d7e40
Justin Benz <jbenz156@gmail.com>
2020-11-06 13:41:18 -0500

    scaffolded project to be where I want it, designed page with different nodes for different pokemon

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,24 @@
 // Write your Character component here
+import Axios from "axios"
+import React,{ useEffect, useState }from "react"
+import '../App.css'
+export default function Character (props) {
+    const[details, setDetails]= useState([])
+    useEffect(() => {
+        Axios
+            .get(`${props.url}`)
+            .then((res) => {
+                console.log(res.data);
+            })
+            .catch((fuzz) => {
+                console.log(fuzz);
+            })
+        },[Character])
+    
+    return(
+        <div className="Char-Container">
+            <h1>{props.name}</h1>
+            <button>Expand Details</button>
+        </div>
+    )
+}
